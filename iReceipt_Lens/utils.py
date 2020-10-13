8b364a2f613b6720e55eedd0fab396b38d1d4782
Ujjwal Gutta <ugutta@asu.edu>
2020-10-12 20:28:51 -0700

    Update utils.py

--- iReceipt_Lens/utils.py
+++ iReceipt_Lens/utils.py
@@ -1,13 +1,26 @@
 import os
 import spacy
+from spacy.util import minibatch, compounding
+from pathlib import Path
+from spacy.gold import GoldParse
+from spacy.scorer import Scorer
+import pandas as pd
 
 
-
-def save_model(output_dir):
-	nlp = spacy.load(output_dir)
+def save_model(model, output_dir):
     if output_dir is not None:
         output_dir = Path(output_dir)
         if not output_dir.exists():
             output_dir.mkdir()
-        nlp.to_disk(output_dir)
+        model.to_disk(output_dir)
         print("Saved model to", output_dir)
+		
+
+def evaluate_model(model,examples):
+    scorer = Scorer()
+    for input_, annot in examples:
+        doc_gold_text = model.make_doc(input_)
+        gold = GoldParse(doc_gold_text, entities=annot['entities'])
+        pred_value = model(input_)
+        scorer.score(pred_value, gold)
+    return scorer.scores
