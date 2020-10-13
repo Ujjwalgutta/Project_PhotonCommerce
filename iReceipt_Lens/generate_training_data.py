e947e9b6b3617fd31627c646123130bc24995fee
Ujjwal Gutta <ugutta@asu.edu>
2020-10-12 20:20:40 -0700

    Update generate_training_data.py

--- iReceipt_Lens/generate_training_data.py
+++ iReceipt_Lens/generate_training_data.py
@@ -4,10 +4,10 @@ import os
 from receipt_data_extract import training_data_tesseract
 from receipt_data_extract import training_data_tectract
 
-train_data_tes = training_data_tesseract(image_file_dir = "receipt_data/input_images/", text_file_dir = "receipt_data/input_text/")
-train_data_tex = training_data_textract(image_file_dir = "receipt_data/input_images/", text_file_dir = "receipt_data/input_text/")
-with open("train_tesseract.pickle",'wb') as f:
+train_data_tes = training_data_tesseract()
+train_data_tex = training_data_textract()
+with open("train_data/train_tesseract.pickle",'wb') as f:
 	pickle.dump(train_data_tes,f)
 
 data = pd.DataFrame(train_data_tex)
-data.to_csv('train_textract.csv',index=False)
+data.to_csv('train_data/train_textract.csv',index=False)
