0b9204c23854a51d9a4fb344bef157b139f45999
Ujjwal Gutta <ugutta@asu.edu>
2020-10-18 18:39:27 -0700

    Create __init__.py

---
+++ iReceipt_Lens/__init__.py
@@ -0,0 +1,6 @@
+from . import utils
+from . import simple_receipt_parser
+from . import generate_training_data
+from . import receipt_data_extract
+from . import train_spacy_ner
+from . import predict
